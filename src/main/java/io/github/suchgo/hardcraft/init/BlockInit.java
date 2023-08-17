19ba6e18d4b958d5138ba95a445cac386aacdcb2
Suchgo <ikruasan@yandex.ru>
2023-08-17 18:31:33 +0300

    Add "silver_ore" block

--- src/main/java/io/github/suchgo/hardcraft/init/BlockInit.java
+++ src/main/java/io/github/suchgo/hardcraft/init/BlockInit.java
@@ -1,7 +1,9 @@
 package io.github.suchgo.hardcraft.init;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,4 +18,10 @@ public class BlockInit {
 
     // Creates a new Block with the id "hardcraft:bush_sticks_block"
     public static final RegistryObject<Block> BUSH_STICKS_BLOCK = BLOCKS.register("bush_sticks_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_LOG).mapColor(MapColor.WOOD)));
+
+    // Creates a new Ore block with the id "hardcraft:silver_ore"
+    public static final RegistryObject<DropExperienceBlock> SILVER_ORE = BLOCKS.register("silver_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_ORE), UniformInt.of(4, 7)));
+
+    // Creates a new Ore block with the id "hardcraft:deepslate_silver_ore"
+    public static final RegistryObject<DropExperienceBlock> DEEPSLATE_SILVER_ORE = BLOCKS.register("deepslate_silver_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(SILVER_ORE.get()), UniformInt.of(4, 7)));
 }
--- src/main/java/io/github/suchgo/hardcraft/init/ItemInit.java
+++ src/main/java/io/github/suchgo/hardcraft/init/ItemInit.java
@@ -15,8 +15,16 @@ public class ItemInit {
 
     // Creates a new resource item with the id "hardcraft:bush_stick_item"
     public static final RegistryObject<Item> BUSH_STICK_ITEM = addToTab(ITEMS.register("bush_stick_item", () -> new Item(new Item.Properties().stacksTo(16))));
+
     // Creates a new BlockItem with the id "hardcraft:bush_sticks_block"
     public static final RegistryObject<BlockItem> BUSH_STICKS_BLOCK_ITEM = addToTab(ITEMS.register("bush_sticks_block", () -> new BlockItem(BlockInit.BUSH_STICKS_BLOCK.get(), new Item.Properties().rarity(Rarity.COMMON))));
+
     // Creates a new axe item with the id "hardcraft:primitive_axe"
     public static final RegistryObject<AxeItem> PRIMITIVE_AXE = addToTab(ITEMS.register("primitive_axe", () -> new AxeItem(Tiers.WOOD, 2, -3.4f, new Item.Properties().durability(50))));
+
+    // Creates a new BlockItem with the id "hardcraft:silver_ore"
+    public static final RegistryObject<BlockItem> SILVER_ORE_ITEM = addToTab(ITEMS.register("silver_ore", () -> new BlockItem(BlockInit.SILVER_ORE.get(), new Item.Properties())));
+
+    // Creates a new BlockItem with the id "hardcraft:silver_ore"
+    public static final RegistryObject<BlockItem> DEEPSLATE_SILVER_ORE_ITEM = addToTab(ITEMS.register("deepslate_silver_ore", () -> new BlockItem(BlockInit.DEEPSLATE_SILVER_ORE.get(), new Item.Properties())));
 }
