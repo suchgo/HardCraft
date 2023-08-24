5fc41e700baecdbe8d8baddf1bf9615194b1f7ab
Suchgo <ikruasan@yandex.ru>
2023-08-24 14:44:13 +0300

    Add fuel item class and test fuel "Peat brick"

--- src/main/java/io/github/suchgo/hardcraft/init/ItemInit.java
+++ src/main/java/io/github/suchgo/hardcraft/init/ItemInit.java
@@ -1,5 +1,6 @@
 package io.github.suchgo.hardcraft.init;
 
+import io.github.suchgo.hardcraft.item.FuelItem;
 import io.github.suchgo.hardcraft.item.MetalDetectorItem;
 import net.minecraft.world.item.*;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,7 +16,8 @@ public class ItemInit {
 
     // Items/Resources
     public static final RegistryObject<Item> BUSH_STICK_ITEM = addToTab(ITEMS.register("bush_stick", () -> new Item(new Item.Properties().stacksTo(16))));
-    public static final RegistryObject<Item> GRASS_THREAD_ITEM = addToTab(ITEMS.register("grass_thread", () -> new Item(new Item.Properties().stacksTo(64))));
+    public static final RegistryObject<Item> GRASS_THREAD_ITEM = addToTab(ITEMS.register("grass_thread", () -> new Item(new Item.Properties())));
+    public static final RegistryObject<Item> PEAT_BRICK_ITEM = addToTab(ITEMS.register("peat_brick", () -> new FuelItem(new Item.Properties(), 200)));
 
     // Tools
     public static final RegistryObject<AxeItem> PRIMITIVE_AXE = addToTab(ITEMS.register("primitive_axe", () -> new AxeItem(Tiers.WOOD, 2, -3.4f, new Item.Properties().durability(50))));
---
+++ src/main/java/io/github/suchgo/hardcraft/item/FuelItem.java
@@ -0,0 +1,20 @@
+package io.github.suchgo.hardcraft.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.jetbrains.annotations.Nullable;
+
+public class FuelItem extends Item {
+
+    private final int burnTime;
+    public FuelItem(Properties properties, int burnTime) {
+        super(properties);
+        this.burnTime = burnTime;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return burnTime;
+    }
+}
