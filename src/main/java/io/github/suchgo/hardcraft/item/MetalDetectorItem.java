468510e02e87158f91ab93744c6e5307490776b0
Suchgo <ikruasan@yandex.ru>
2023-08-24 15:07:29 +0300

    Add tooltip to "Metal detector" item

--- src/main/java/io/github/suchgo/hardcraft/item/MetalDetectorItem.java
+++ src/main/java/io/github/suchgo/hardcraft/item/MetalDetectorItem.java
@@ -6,13 +6,18 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MetalDetectorItem extends Item {
@@ -59,5 +64,12 @@ public class MetalDetectorItem extends Item {
         };
 
         return Arrays.asList(valuableBlocks).contains(block);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponent, TooltipFlag pToolTipFlag) {
+        pTooltipComponent.add(Component.translatable("tooltip.hardcraft.metal_detector"));
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponent, pToolTipFlag);
     }
 }
